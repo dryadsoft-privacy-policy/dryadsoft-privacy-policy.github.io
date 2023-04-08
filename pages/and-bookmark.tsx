5ca37e65cb65bad21f342dd2da17bccb4408bb5c
dryadsoft <dryadsoft@gmail.com>
2023-04-08 16:15:10 +0900

    ios 개인정보처리방침 추가

--- pages/and-bookmark.tsx
+++ pages/and-bookmark.tsx
@@ -1,15 +1,16 @@
 import type { NextPage } from "next";
+import Description from "../components/Description";
 
-const Home: NextPage = () => {
+const BookMark: NextPage = () => {
   const componayName = "DryadSoft";
   const appName = "북마크(BookMark)";
   const firstRegDate = "2022-03-09";
   const lastUpdateDate = "2022-03-09";
   const email = "dryadsoft@gmail.com";
   return (
-    <div className="container bg-gray-200 flex flex-col justify-center m-auto text-lg px-10">
+    <div className="container bg-gray-200 flex flex-col justify-center m-auto text-lg px-4 sm:px-10">
       <h1 className="text-2xl font-bold mx-auto my-4">개인정보처리방침</h1>
-      <div className="mb-4">
+      <div className="mb-2">
         {componayName}은(는) {appName} 앱을(를) 무료로 사용할 수 있도록
         만들었습니다.
       </div>
@@ -28,72 +29,61 @@ const Home: NextPage = () => {
         달리 정의되지 않는 한 ‘이용 약관’에서 액세스 할 수있는 이용 약관과
         동일한 의미를 갖습니다.
       </div>
-
-      <h2 className="text-xl font-bold">• 정보 수집 및 사용</h2>
-      <div className="mb-8">
+      <Description title="정보 수집 및 사용">
         더 나은 경험을 위해 서비스를 사용하는 동안 특정 개인 식별 정보를
         제공해야 할 수도 있습니다. 우리가 요청한 정보는 이 개인 정보 취급 방침에
         설명 된대로 당사에 보유되고 사용됩니다
-      </div>
-      <div>
+        <br />
         앱은 귀하를 식별하는 데 사용되는 정보를 수집 할 수있는 제 3 자 서비스를
         사용합니다. 앱에서 사용하는 타사 서비스 제공 업체의 개인 정보 취급
         방침에 대한 링크
-        <div>
-          <div>
-            <a href="https://policies.google.com/privacy">
-              <span className="text-blue-500">Google Play 서비스</span>
-            </a>
-          </div>
-        </div>
+        <br />
+        <a href="https://policies.google.com/privacy">
+          <span className="text-blue-500">Google Play 서비스</span>
+        </a>
+        <br />
         서비스를 이용하는데 필요한 최소한의 식별정보(로그인ID)만을 이용하며
         이름, 주소, 이메일정보등은 외부(서버등)에 절대 수집하거나 저장하지
         않습니다
-      </div>
-
-      <h2 className="text-xl font-bold">• 다른 사이트 링크</h2>
-      <div className="mb-8">
+      </Description>
+      <Description title="다른 사이트 링크">
         이 서비스에는 다른 사이트에 대한 링크가 포함되어있을 수 있습니다. 제 3
         자 링크를 클릭하면 해당 사이트로 연결됩니다. 이러한 외부 사이트는{" "}
         {componayName}에서 운영하지 않습니다. 따라서 이 웹 사이트의 개인 정보
         취급 방침을 검토하는 것이 좋습니다. 우리는 제3자 사이트 또는 서비스의
         내용, 개인 정보 보호 정책 또는 관행에 대해 어떠한 통제도하지 않으며
         책임을지지 않습니다.
-      </div>
-
-      <h2 className="text-xl font-bold">• 어린이 개인 정보 보호</h2>
-      <div className="mb-8">
+      </Description>
+      <Description title="어린이 개인 정보 보호">
         이 서비스는 전연령을 대상으로 합니다. 서비스를 이용하는데 필요한
         최소한의 식별정보(로그인ID)만을 이용하며 이름, 주소, 이메일정보등은
         외부(서버등)에 절대 수집하거나 저장하지 않습니다. 부모님 또는 보호자이며
         자녀가 개인 정보를 제공 한 사실을 알고 계시다면 저희에게 연락하여 필요한
         조치를 취할 수 있도록하십시오.
-      </div>
-
-      <h2 className="text-xl font-bold">• 서비스제공국가</h2>
-      <div>해당서비스의 내용은 대한민국을 주요대상으로 합니다</div>
-      <div className="mb-8">
+      </Description>
+      <Description title="서비스제공국가">
+        해당서비스의 내용은 대한민국을 주요대상으로 합니다
+        <br />
         This app is currently only available in the Korea.
-      </div>
-
-      <h2 className="text-xl font-bold">• 이 개인 정보 보호 정책의 변경</h2>
-      <div>
+      </Description>
+      <Description title="이 개인 정보 보호 정책의 변경">
         개인 정보 보호 정책은 수시로 업데이트 될 수 있습니다. 따라서 이 페이지를
         정기적으로 검토하여 변경 사항을 확인하는 것이 좋습니다. 이 페이지에
         새로운 개인 정보 취급 방침을 게시함으로써 변경 사항을 알려 드리겠습니다.
         이 변경 사항은이 페이지에 게시 된 후 즉시 적용됩니다.
-      </div>
-      <div>최초등록 : {firstRegDate}</div>
-      <div className="mb-8">최종수정 : {lastUpdateDate}</div>
-
-      <h2 className="text-xl font-bold">• 서비스제공자 연락처</h2>
-      <div>
+        <br />
+        최초등록 : {firstRegDate}
+        <br />
+        최종수정 : {lastUpdateDate}
+      </Description>
+      <Description title="서비스제공자 연락처">
         개인 정보 보호 정책에 관한 질문이나 제안 사항이 있으시면 언제든지
         저희에게 연락하십시오.
-      </div>
-      <div>이메일 주소 : {email}</div>
+        <br />
+        이메일 주소 : {email}
+      </Description>
     </div>
   );
 };
 
-export default Home;
+export default BookMark;
